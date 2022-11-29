d4f8f716419ecd7281fde40c3cc989bac1ce89a2
DangAnh982001 <danganh982001@gmail.com>
2022-11-29 16:00:02 +0700

    DTA_0037

---
+++ Data/ApplicationDbcontext.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Nhom13.Models;
+
+    public class ApplicationDbcontext : DbContext
+    {
+        public ApplicationDbcontext (DbContextOptions<ApplicationDbcontext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Nhom13.Models.Nhacungcap> Nhacungcap { get; set; } = default!;
+
+        public DbSet<Nhom13.Models.Sanpham> Sanpham { get; set; } = default!;
+
+        public DbSet<Nhom13.Models.Nhanvien> Nhanvien { get; set; } = default!;
+
+        public DbSet<Nhom13.Models.Nhaphang> Nhaphang { get; set; } = default!;
+    }
